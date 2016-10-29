83248c21a8708aa8c25e0caadc4a274aa53b6736
Tobias Bengfort <tobias.bengfort@gmx.net>
2016-10-29 18:01:55 +0200

    test easter

--- test.py
+++ test.py
@@ -11,6 +11,41 @@ import cal
 TODAY = date.today()
 
 
+class TestEaster(unittest.TestCase):
+	def test_easter(self):
+		self.assertEqual(cal.easter(2000), date(2000, 4, 23))
+		self.assertEqual(cal.easter(2001), date(2001, 4, 15))
+		self.assertEqual(cal.easter(2002), date(2002, 3, 31))
+		self.assertEqual(cal.easter(2003), date(2003, 4, 20))
+		self.assertEqual(cal.easter(2004), date(2004, 4, 11))
+		self.assertEqual(cal.easter(2005), date(2005, 3, 27))
+		self.assertEqual(cal.easter(2006), date(2006, 4, 16))
+		self.assertEqual(cal.easter(2007), date(2007, 4, 8))
+		self.assertEqual(cal.easter(2008), date(2008, 3, 23))
+		self.assertEqual(cal.easter(2009), date(2009, 4, 12))
+		self.assertEqual(cal.easter(2010), date(2010, 4, 4))
+		self.assertEqual(cal.easter(2011), date(2011, 4, 24))
+		self.assertEqual(cal.easter(2012), date(2012, 4, 8))
+		self.assertEqual(cal.easter(2013), date(2013, 3, 31))
+		self.assertEqual(cal.easter(2014), date(2014, 4, 20))
+		self.assertEqual(cal.easter(2015), date(2015, 4, 5))
+		self.assertEqual(cal.easter(2016), date(2016, 3, 27))
+		self.assertEqual(cal.easter(2017), date(2017, 4, 16))
+		self.assertEqual(cal.easter(2018), date(2018, 4, 1))
+		self.assertEqual(cal.easter(2019), date(2019, 4, 21))
+		self.assertEqual(cal.easter(2020), date(2020, 4, 12))
+		self.assertEqual(cal.easter(2021), date(2021, 4, 4))
+		self.assertEqual(cal.easter(2022), date(2022, 4, 17))
+		self.assertEqual(cal.easter(2023), date(2023, 4, 9))
+		self.assertEqual(cal.easter(2024), date(2024, 3, 31))
+		self.assertEqual(cal.easter(2025), date(2025, 4, 20))
+		self.assertEqual(cal.easter(2026), date(2026, 4, 5))
+		self.assertEqual(cal.easter(2027), date(2027, 3, 28))
+		self.assertEqual(cal.easter(2028), date(2028, 4, 16))
+		self.assertEqual(cal.easter(2029), date(2029, 4, 1))
+		self.assertEqual(cal.easter(2030), date(2030, 4, 21))
+
+
 class TestParseDate(unittest.TestCase):
 	def test_dd(self):
 		tpl = cal.parse_date('13')
