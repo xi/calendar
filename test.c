3d9de7ab5542868ebb7c14edb01c3e54535dd658
Tobias Bengfort <tobias.bengfort@posteo.de>
2018-11-19 16:07:57 +0100

    wip: test

---
+++ test.c
@@ -0,0 +1,28 @@
+#include "calendar.c"
+
+#define mu_assert(message, test) do { if (!(test)) return message; } while (0)
+#define mu_run_test(test) do { char *message = test(); tests_run++; if (message) return message; } while (0)
+
+int tests_run = 0;
+
+static char *test_foo() {
+	mu_assert("fail!", false);
+}
+
+static char * all_tests() {
+	mu_run_test(test_foo);
+	return 0;
+}
+
+int main(int argc, char **argv) {
+	char *result = all_tests();
+	if (result != 0) {
+		printf("%s\n", result);
+	}
+	else {
+		printf("ALL TESTS PASSED\n");
+	}
+	printf("Tests run: %d\n", tests_run);
+
+	return result != 0;
+}
